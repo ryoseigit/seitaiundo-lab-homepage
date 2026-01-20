4635b1b66b0e47b17ee1e37b9e443e37fe8202d5
ryoseigit <ur.1013.py@gmail.com>
2026-01-21 01:55:47 +0900

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    // content の中に './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}' があるか確認
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
